eead7155d43474f3bed5cb6a92588de8ade29fb1
Josh Justice <me@codingitwrong.com>
2019-02-06 08:19:31 -0500

    Fix onSend error

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import NewMessageForm from './NewMessageForm';
 
 class App extends Component {
+  handleSend = (newMessage) => {
+  }
+
   render() {
     return (
       <div>
-        <NewMessageForm />
+        <NewMessageForm onSend={this.handleSend} />
       </div>
     );
   }
