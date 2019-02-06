a17645160d6422c21a611dd5b1e65256c35ab293
Josh Justice <me@codingitwrong.com>
2019-02-06 08:15:00 -0500

    Make component controlled

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 export default class NewMessageForm extends Component {
+  state = { inputText: '' }
+
+  handleTextChange = (event) => {
+    this.setState({ inputText: event.target.value });
+  }
+
   render() {
+    const { inputText } = this.state;
+
     return (
       <div>
         <input
           type="text"
           data-testid="messageText"
+          value={inputText}
+          onChange={this.handleTextChange}
         />
         <button
           data-testid="sendButton"
