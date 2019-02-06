7ac6e4829f9b72e7740ebff752a4f2a08f48f949
Josh Justice <me@codingitwrong.com>
2019-02-06 08:19:56 -0500

    Store messages in app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 import NewMessageForm from './NewMessageForm';
 
 class App extends Component {
+  state = { messages: [] };
+
   handleSend = (newMessage) => {
+    this.setState(state => ({
+      messages: [newMessage, ...state.messages],
+    }));
   }
 
   render() {
