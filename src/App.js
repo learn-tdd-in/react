8614b3a1af609d66017fd7661c039fae73d27516
Josh Justice <me@codingitwrong.com>
2019-02-06 08:03:58 -0500

    Add empty NewMessageForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import NewMessageForm from './NewMessageForm';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <NewMessageForm />
       </div>
     );
   }
