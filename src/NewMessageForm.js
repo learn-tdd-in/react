1f85ec704504241aaa5d3fdb66a91169db9d6253
Josh Justice <josh.justice@accesscfa.com>
2019-11-08 07:53:43 -0500

    Add send button

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -4,6 +4,7 @@ const NewMessageForm = () => {
   return (
     <div>
       <input type="text" data-testid="messageText" />
+      <button data-testid="sendButton">Send</button>
     </div>
   );
 };
