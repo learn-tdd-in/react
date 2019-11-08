5471a170c85b3c0a6827daad2ed0be7c4b671283
Josh Justice <josh.justice@accesscfa.com>
2019-11-08 08:00:55 -0500

    Clear text field

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewMessageForm = () => {
+  const [inputText, setInputText] = useState('');
+
+  const handleTextChange = event => {
+    setInputText(event.target.value);
+  };
+
+  const handleSend = () => {
+    setInputText('');
+  };
+
   return (
     <div>
-      <input type="text" data-testid="messageText" />
-      <button data-testid="sendButton">Send</button>
+      <input
+        type="text"
+        data-testid="messageText"
+        value={inputText}
+        onChange={handleTextChange}
+      />
+      <button data-testid="sendButton" onClick={handleSend}>
+        Send
+      </button>
     </div>
   );
 };
