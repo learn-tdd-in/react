1097ceccc23cfd448fd7443f1324247409c3ba5b
Josh Justice <me@codingitwrong.com>
2020-10-17 14:11:56 -0400

    Add message text field

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import NewMessageForm from './NewMessageForm';
 
 const App = () => {
   return (
     <div>
+      <NewMessageForm />
     </div>
   );
 };
---
+++ src/NewMessageForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NewMessageForm = () => {
+  return (
+    <div>
+      <input
+        type="text"
+        data-testid="messageText"
+      />
+    </div>
+  );
+};
+
+export default NewMessageForm;
