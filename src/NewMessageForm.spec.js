e7e51b8949cb017352f43c6655ea37753fc9a76d
Josh Justice <me@codingitwrong.com>
2022-04-16 07:19:01 -0400

    Call send handler

--- src/NewMessageForm.spec.js
+++ src/NewMessageForm.spec.js
@@ -4,8 +4,12 @@ import NewMessageForm from './NewMessageForm';
 
 describe('NewMessageForm', () => {
   describe('clicking the send button', () => {
+    let sendHandler;
+
     beforeEach(async () => {
-      render(<NewMessageForm />);
+      sendHandler = jest.fn().mockName('sendHandler');
+
+      render(<NewMessageForm onSend={sendHandler} />);
 
       await userEvent.type(
         screen.getByTestId('messageText'),
@@ -16,6 +20,10 @@ describe('NewMessageForm', () => {
 
     it('clears the text field', () => {
       expect(screen.getByTestId('messageText').value).toEqual('');
+    });
+
+    it('calls the send handler', () => {
+      expect(sendHandler).toHaveBeenCalledWith('New message');
     });
   });
 });
