2f03ff9059e5c8236fdf8e9b81fe8c80a45dc6ea
Josh Justice <me@codingitwrong.com>
2022-04-16 07:27:59 -0400

    Store messages in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import {useState} from 'react';
 import NewMessageForm from './NewMessageForm';
 
 export default function App() {
-  function handleSend() {}
+  const [messages, setMessages] = useState([]);
+  function handleSend(newMessage) {
+    setMessages([newMessage, ...messages]);
+  }
 
   return <NewMessageForm onSend={handleSend} />;
 }
