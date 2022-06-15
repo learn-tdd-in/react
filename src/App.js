3d602b66e61b144b3f6061f2f9ad0f386a31410e
Josh Justice <jjustice@justworks.com>
2022-06-15 12:55:09 -0400

    Display messages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import {useState} from 'react';
+import MessageList from './MessageList';
 import NewMessageForm from './NewMessageForm';
 
 export default function App() {
-  function handleSend() {}
+  const [messages, setMessages] = useState([]);
+  function handleSend(newMessage) {
+    setMessages([newMessage, ...messages]);
+  }
 
-  return <NewMessageForm onSend={handleSend} />;
+  return (
+    <>
+      <NewMessageForm onSend={handleSend} />
+      <MessageList data={messages} />
+    </>
+  );
 }
---
+++ src/MessageList.js
@@ -0,0 +1,9 @@
+export default function MessageList({data}) {
+  return (
+    <ul>
+      {data.map(message => (
+        <li key={message}>{message}</li>
+      ))}
+    </ul>
+  );
+}
