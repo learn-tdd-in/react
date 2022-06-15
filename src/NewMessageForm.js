9b2621ab27d478d21defaa0a52e68f7cb677c5a6
Josh Justice <jjustice@justworks.com>
2022-06-15 12:52:00 -0400

    Add send button

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,3 +1,8 @@
 export default function NewMessageForm() {
-  return <input type="text" data-testid="messageText" />;
+  return (
+    <>
+      <input type="text" data-testid="messageText" />
+      <button data-testid="sendButton">Send</button>
+    </>
+  );
 }
