0eb8c22fb8250b350ab75a225a71d8755cff0aeb
Josh Justice <jjustice@justworks.com>
2022-06-15 12:48:11 -0400

    Autoformat files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,15 @@
-
-        module.exports = {
-          extends: ['react-app', 'prettier'],
-          plugins: [
-            'prettier',
-            'cypress',
-          ],
-          env: {'cypress/globals': true},
-          rules: {
-            'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
-            'no-duplicate-imports': 'error',
-            'prettier/prettier': 'warn',
-            quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
-            'sort-imports': [
-              'warn',
-              {ignoreDeclarationSort: true, ignoreMemberSort: false},
-            ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
-          },
-        };
-      
+module.exports = {
+  extends: ['react-app', 'prettier'],
+  plugins: ['prettier', 'cypress'],
+  env: {'cypress/globals': true},
+  rules: {
+    'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
+    'prettier/prettier': 'warn',
+    quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
+    'sort-imports': [
+      'warn',
+      {ignoreDeclarationSort: true, ignoreMemberSort: false},
+    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+  },
+};
